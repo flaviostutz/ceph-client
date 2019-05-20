2208a909198ba1b499fba5b962bb8a429e753f06
flaviostutz <flaviostutz@gmail.com>
2019-05-20 13:15:01 -0300

    adding kernel version check

--- status.sh
+++ status.sh
@@ -2,6 +2,13 @@
 set -e
 # set -x
 
+kv=$(uname -r)
+vc=$(semver compare $kv 4.5.2)
+if [ "$vc" == "-1" ]; then
+    echo "The Linux host kernel version must be greater or equal 4.5.2. current version=$kv"
+    exit 1
+fi
+
 echo "This is a sample script. Extend this image and create a startup.sh script that calls './initialize.sh' for the client to be setup'."
 
 ceph -w&
